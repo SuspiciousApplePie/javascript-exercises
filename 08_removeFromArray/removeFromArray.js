71d89474a4599f8aef4bcaed1c2ee2ff555d39e6
Shishi <andreiusita6@gmail.com>
2025-06-30 00:11:02 +0800

    feat: Finish activity

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(numbers, ...removals) {
+    for (let i = 0; i < numbers.length; i++) {
+        if (removals.includes(numbers[i])) {
+            numbers.splice(i, 1);
+            i--;
+        }
+    }
+    return numbers;
 };
 
 // Do not edit below this line
