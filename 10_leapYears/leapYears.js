724bc3c6e08193dcf3fcd9d8805753559ea03c7d
Shishi <andreiusita6@gmail.com>
2025-07-01 18:27:30 +0800

    feat: Finish the activity.

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    const divisibleByFour = year % 4 === 0;
+    const isCentury = year % 100 === 0;
+    const isFourHundred = year % 400 === 0;
+    if ((divisibleByFour 
+        && !isCentury) 
+        || isFourHundred) return true;
+    return false;
 };
 
 // Do not edit below this line
