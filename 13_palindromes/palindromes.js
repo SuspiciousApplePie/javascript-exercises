a6b02072aa2d6f84e586b7e44fab0b1dbb076122
Shishi <andreiusita6@gmail.com>
2025-08-11 17:36:07 +0800

    feat: Finish activity

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+ arr = word.replaceAll(/[^A-Za-z0-9/s]/g, '').toLowerCase().split('');
+ let reverse = arr.reduceRight((acc, curr) => {
+    acc.push(curr);
+    return acc;
+ }, [])
+ return reverse.join('') === arr.join('');
 };
 
 // Do not edit below this line
