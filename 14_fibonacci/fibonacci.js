8173898dbdb01aa2cfa9c2f2c44820c60a152042
Shishi <andreiusita6@gmail.com>
2025-08-13 21:06:50 +0800

    feat: FInish activity 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = Number(num);
+    let prev = 0;
+    let curr = 1;
+    let total = 0; // Accumulator
+    if (num < 0) return 'OOPS';
+    if (num === 1) return curr;
+    for (i = 2; i <= num; i++) {
+        total = curr + prev;
+        prev = curr;
+        curr = total;
+    }
+    return total;
 };
 
 // Do not edit below this line
