4ed09bc02fa1b016d3417d7db346921abe227881
Shishi <andreiusita6@gmail.com>
2025-08-13 22:26:59 +0800

    feat: Finisg act 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,15 @@
-const findTheOldest = function() {
 
+const findTheOldest = function(arr) {
+    return arr.reduce((acc, curr) => {
+        if (!curr['yearOfDeath']) curr['yearOfDeath'] = 2025;
+        let age = curr['yearOfDeath'] - curr['yearOfBirth'];
+        if (age > acc['age']) {
+            acc['name'] = curr['name']
+            acc['age'] = age;
+        }
+        return acc;
+    }, 
+    {age: 0});
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
