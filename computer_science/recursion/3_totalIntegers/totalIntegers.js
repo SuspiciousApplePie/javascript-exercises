f4f14c16b507f66da946acd46b80db68eeff28de
SuspiciousApplePie <andreiusita6@gmail.com>
2026-04-26 17:17:03 +0800

    Finish totalIntegers

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,18 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function (numbers) {
+  if (typeof numbers !== "object") return;
+  if (!Array.isArray(numbers)) numbers = Object.values(numbers);
+  return numbers.reduce((totalAmountOfIntegers, currentInteger) => {
+    if (Number.isInteger(currentInteger)) ++totalAmountOfIntegers;
+    if (
+      Array.isArray(currentInteger) ||
+      (typeof currentInteger === "object" &&
+        !Array.isArray(currentInteger) &&
+        currentInteger !== null)
+    )
+      totalAmountOfIntegers += totalIntegers(currentInteger);
+    return totalAmountOfIntegers;
+  }, 0);
 };
-  
+
 // Do not edit below this line
 module.exports = totalIntegers;
