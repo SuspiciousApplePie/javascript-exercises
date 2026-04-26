7bfa56978b8effadaf0d9090d7becfd58bec4fa2
SuspiciousApplePie <andreiusita6@gmail.com>
2026-04-26 17:16:47 +0800

    Finish contains

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,13 @@
-const contains = function() {
-  
+const contains = function (obj, val) {
+  for (let subObj of Object.values(obj)) {
+    if (Object.is(val, subObj)) return true;
+    if (typeof subObj === "object" && subObj !== null) {
+      const isContain = contains(subObj, val);
+      if (isContain) return true;
+    }
+  }
+  return false;
 };
-  
+
 // Do not edit below this line
 module.exports = contains;
