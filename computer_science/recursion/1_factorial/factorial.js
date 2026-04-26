aedb75820b4fed7723b4a172d4d6fd5c068a0740
SuspiciousApplePie <andreiusita6@gmail.com>
2026-04-26 13:09:57 +0800

    Finish factorial

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,5 +1,7 @@
-const factorial = function() {
-
+const factorial = function (number) {
+  if (number === 0) return 1;
+  else if (number < 0 || !Number.isInteger(number)) return;
+  return number * factorial(number - 1);
 };
 
 // Do not edit below this line
