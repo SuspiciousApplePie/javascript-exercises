469185b824c1a3671f076abf9e08cbd8b2d6b55b
SuspiciousApplePie <andreiusita6@gmail.com>
2026-04-29 23:02:44 +0800

    Finish permutations

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,6 +1,24 @@
-const permutations = function() {
-  
+const permutations = function (numbers) {
+  let result = [];
+  if (!numbers.length) {
+    result.push([]);
+    return result;
+  }
+  for (let i = 0; i <= numbers.length - 1; i++) {
+    const numberCopy = numbers.slice();
+    numberCopy.splice(i, 1);
+    const permutated = permutations(numberCopy);
+    permutated.forEach((index) => {
+      let permutatedList = [];
+      console.log(`push the ${numbers[i]} to array`);
+      permutatedList.push(numbers[i]);
+      console.log(`combine ${numbers[i]} and ${index} to the array`);
+      permutatedList = permutatedList.concat(index);
+      result.push(permutatedList);
+    });
+  }
+  return result;
 };
-  
+
 // Do not edit below this line
 module.exports = permutations;
