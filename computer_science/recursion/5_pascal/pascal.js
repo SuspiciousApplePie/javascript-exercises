a385c8bfa8ec5ee220baea0e1b7e59a169db382a
SuspiciousApplePie <andreiusita6@gmail.com>
2026-04-30 21:46:06 +0800

    Finish pascal

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,6 +1,18 @@
-const pascal = function() {
-  
+const pascal = function (layer) {
+  const triangle = [];
+  if (layer === 1) {
+    triangle.push(layer);
+    return triangle;
+  }
+  const pascalled = pascal(layer - 1);
+  for (let i = 0; i <= layer - 1; i++) {
+    if (i === 0 || i === layer) triangle.push(1);
+    else {
+      triangle.push(pascalled[i - 1] + (pascalled[i] || 0));
+    }
+  }
+  return triangle;
 };
-  
+
 // Do not edit below this line
 module.exports = pascal;
